b73b26be517418209d7cccf9789738afc8505d1b
Alexgta <9720560@gmail.com>
2019-04-28 18:16:29 -0400

    Add EventEmitter (no design)

--- task-planner/src/app/task-list/list-row/list-row.component.ts
+++ task-planner/src/app/task-list/list-row/list-row.component.ts
@@ -1,18 +1,25 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list-row',
   templateUrl: './list-row.component.html',
   styleUrls: ['./list-row.component.css']
 })
+
 export class ListRowComponent implements OnInit {
 
   // @Output() deleteTaskEmitter = new EventEmitter<string>();
   @Output() deleteTaskEmitter = new EventEmitter()
   @Output() valueChange = new EventEmitter();
+
+  /////////////////////////
+  @Input() result = '';
+  @Output() clicked = new EventEmitter<string>();
+  /////////////////////////
+
   Counter = 0;
 
-  name = 'Name -10';
+  name = 'List-row-comp -10';
   category = 'Type 10';
   dateStart = '18:15 08-10-2018';
   dateEnd = '20:15 08-10-2018';
@@ -26,6 +33,10 @@ export class ListRowComponent implements OnInit {
   valueChanged() {
     this.Counter = this.Counter + 1;
     this.deleteTaskEmitter.emit(this.Counter);
+  }
+
+  onClick(searchTerm) {
+    this.clicked.emit(searchTerm);
   }
 
   constructor() { }
--- task-planner/src/app/task-list/task-list.component.ts
+++ task-planner/src/app/task-list/task-list.component.ts
@@ -8,52 +8,88 @@ import { Component, OnInit } from '@angular/core';
 
 export class TaskListComponent implements OnInit {
 
-  constructor() { }
-
   products = [
     {
       name: 'Call Tomm -1',
       category: 'Not urgent',
       dateStart: 'May 15 2020',
       dateEnd: 'May 22 2020',
-      status: 'To do'
+      status: 'To do',
+      p_id: 1, pname: 'omega', pprice: 101, pimg: '1', soh: 5
     },
     {
       name: 'Visit friend -2',
       category: 'Not urgent',
       dateStart: 'June 18 2020',
       dateEnd: 'June 28 2020',
-      status: 'To do'
+      status: 'To do',
+      p_id: 2, pname: 'timex', pprice: 102, pimg: '2', soh: 10
     },
     {
       name: 'Fish -3',
       category: 'Urgent',
       dateStart: 'Apr 15 2020',
       dateEnd: 'Apr 25 2020',
-      status: 'Done'
+      status: 'Done',
+      p_id: 3, pname: 'titan', pprice: 103, pimg: '3', soh: 15
     },
     {
       name: 'Call Sam -4',
       category: 'Not urgent',
       dateStart: 'May 16 2020',
       dateEnd: 'May 26 2020',
-      status: 'Done'
+      status: 'Done',
+      p_id: '4', pname: 'fossil', pprice: 104, pimg: '4', soh: 20
     },
     {
       name: 'Name -5',
       category: 'Type 110',
       dateStart: '18:15 08-10-2019',
       dateEnd: '20:15 08-10-2019',
-      status: 'Done'
+      status: 'Done',
+      p_id: 5, pname: 'rolex', pprice: 105, pimg: '5', soh: 25
     },
     {
       name: 'Name -6',
       category: 'Type 102',
       dateStart: '18:15 08-10-2019',
       dateEnd: '20:15 08-10-2019',
-      status: 'Over Due'
+      status: 'Over Due',
+      p_id: 6, pname: 'Cob-brig', pprice: 106, pimg: '6', soh: 30
     }
   ];
+
+  products1 = [
+  ];
+  products2 = [
+    {
+      name: 'Name -26',
+      category: 'Type 122',
+      dateStart: '18:15 08-10-2020',
+      dateEnd: '20:15 08-10-2019',
+      status: 'To Do',
+      p_id: 6, pname: 'Cob-brig', pprice: 106, pimg: '6', soh: 30
+    }
+  ];
+
+  constructor() {
+    // @ts-ignore
+    this.products1 = this.products;
+  }
+  /////////////////////////////////
+
+  value = 'Watch';
+  onClicked(value) {
+    if (value !== '') {
+      this.products1 = this.products.filter((task) => task.status.startsWith(value));
+    } else {
+      this.products1 = this.products;
+    }
+  }
+
+  deleteTask(name: string) {
+    console.log('Task ' + name + ' Deleted');
+  }
 
   getTasksAmountByStatus(status) {
     return this.products.filter((task) => task.status === status).length;
@@ -63,12 +99,8 @@ export class TaskListComponent implements OnInit {
     return this.products.length;
   }
 
-  /*deleteTaskFromArray(name: string) {
-    console.log('Task' + name + 'Deleted');
-  }*/
-
   deleteTaskFromArray($event) {
-    console.log('Task' + $event + 'Deleted');
+    console.log('You clicked ' + $event + ' times');
   }
 
   displayCounter(count) {
